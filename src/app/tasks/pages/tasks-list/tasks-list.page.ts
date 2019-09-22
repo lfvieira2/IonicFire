a2c4c86696cb148834d7c20d665c47a161a07c49
Luiz Filipe <luiz.filipe.vieira@hotmail.com>
2019-09-21 21:22:15 -0300

    Exportando e importando components

--- src/app/tasks/pages/tasks-list/tasks-list.page.ts
+++ src/app/tasks/pages/tasks-list/tasks-list.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-tasks-list',
   templateUrl: './tasks-list.page.html',
-  styleUrls: ['./tasks-list.page.scss'],
+  styleUrls: ['./tasks-list.page.scss']
 })
 export class TasksListPage implements OnInit {
+  tasks$: Observable<Task[]>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.tasks$ = of([
+      { id: 'asdasd', title: 'Aprender Ionic', done: false },
+      { id: 'asfasf', title: 'Aprender Firestore', done: false }
+    ]);
   }
-
 }
