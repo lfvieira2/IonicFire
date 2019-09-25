6732362a2dcb187b1ccbd7fefc378b07851066f0
Luiz Filipe <luiz.filipe.vieira@hotmail.com>
2019-09-25 19:17:45 -0300

    Criando novas  tarefas

--- src/app/tasks/pages/task-save/task-save.page.ts
+++ src/app/tasks/pages/task-save/task-save.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { TasksService } from '../../services/tasks.service';
 
 @Component({
   selector: 'app-task-save',
   templateUrl: './task-save.page.html',
-  styleUrls: ['./task-save.page.scss'],
+  styleUrls: ['./task-save.page.scss']
 })
 export class TaskSavePage implements OnInit {
+  taskForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private tasksService: TasksService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.createForm();
+  }
+  private createForm(): void {
+    this.taskForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      done: [false]
+    });
   }
 
+  async onSubmit(): Promise<void> {
+    try {
+      const task = await this.tasksService.create(this.taskForm.value);
+      console.log('Task created ', task);
+    } catch (error) {
+      console.log('Error save Task:', error);
+    }
+  }
 }
