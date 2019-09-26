2eb0ba09b0735aacaee286304a54ea337ba23ff9
Luiz Filipe <luiz.filipe.vieira@hotmail.com>
2019-09-26 19:00:31 -0300

    Apresentar msg de erro

--- src/app/tasks/pages/task-save/task-save.page.ts
+++ src/app/tasks/pages/task-save/task-save.page.ts
@@ -40,6 +40,9 @@ export class TaskSavePage implements OnInit {
       this.navCtrl.navigateBack('/tasks');
     } catch (error) {
       console.log('Error save Task:', error);
+      await this.overlayService.toast({
+        message: error.message
+      });
     } finally {
       loading.dismiss();
     }
