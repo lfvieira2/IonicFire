caa3bfca4a9e7b1292f67a702a285e00d770980a
Luiz Filipe <luiz.filipe.vieira@hotmail.com>
2019-09-26 18:59:18 -0300

    Mostrar loading

--- src/app/tasks/pages/task-save/task-save.page.ts
+++ src/app/tasks/pages/task-save/task-save.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
 
+import { OverlayService } from 'src/app/core/services/overlay.service';
 import { TasksService } from '../../services/tasks.service';
 
 @Component({
@@ -15,7 +16,8 @@ export class TaskSavePage implements OnInit {
   constructor(
     private fb: FormBuilder,
     private tasksService: TasksService,
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private overlayService: OverlayService
   ) {}
 
   ngOnInit(): void {
@@ -29,12 +31,17 @@ export class TaskSavePage implements OnInit {
   }
 
   async onSubmit(): Promise<void> {
+    const loading = await this.overlayService.loading({
+      message: 'Saving...'
+    });
     try {
       const task = await this.tasksService.create(this.taskForm.value);
       console.log('Task created ', task);
       this.navCtrl.navigateBack('/tasks');
     } catch (error) {
       console.log('Error save Task:', error);
+    } finally {
+      loading.dismiss();
     }
   }
 }
