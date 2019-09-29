8c9585ea865df0f65c66d77d3d52381cda7bcde7
Luiz Filipe <luiz.filipe.vieira@hotmail.com>
2019-09-29 15:03:54 -0300

    ajustes

--- src/app/tasks/components/task-item/task-item.component.spec.ts
+++ src/app/tasks/components/task-item/task-item.component.spec.ts
@@ -1,7 +1,7 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TaskItemComponent } from './task-item.component';
+import { TaskItemComponent } from './tasks-item.component';
 
 describe('TaskItemComponent', () => {
   let component: TaskItemComponent;
