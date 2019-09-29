5b04014dc478a3769eab4e597f515bbaa6b0a805
Luiz Filipe <luiz.filipe.vieira@hotmail.com>
2019-09-29 15:20:28 -0300

    exibindo tarefa para edição

--- src/app/tasks/pages/task-save/task-save.page.ts
+++ src/app/tasks/pages/task-save/task-save.page.ts
@@ -1,9 +1,11 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
 
 import { OverlayService } from 'src/app/core/services/overlay.service';
 import { TasksService } from '../../services/tasks.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-task-save',
@@ -12,17 +14,40 @@ import { TasksService } from '../../services/tasks.service';
 })
 export class TaskSavePage implements OnInit {
   taskForm: FormGroup;
+  pageTitle = '...';
+  taskId: string = undefined;
 
   constructor(
     private fb: FormBuilder,
     private tasksService: TasksService,
     private navCtrl: NavController,
+    private route: ActivatedRoute,
     private overlayService: OverlayService
   ) {}
 
   ngOnInit(): void {
     this.createForm();
+    this.init();
   }
+
+  init(): void {
+    const taskId = this.route.snapshot.paramMap.get('id');
+    if (!taskId) {
+      this.pageTitle = 'Create Task';
+      return;
+    }
+    this.taskId = taskId;
+    console.log('taskId: ', taskId);
+    this.pageTitle = 'Edit Task';
+    this.tasksService
+      .get(taskId)
+      .pipe(take(1))
+      .subscribe(({ title, done }) => {
+        this.taskForm.get('title').setValue(title);
+        this.taskForm.get('done').setValue(done);
+      });
+  }
+
   private createForm(): void {
     this.taskForm = this.fb.group({
       title: ['', [Validators.required, Validators.minLength(3)]],
