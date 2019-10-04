2684aef6215e78fa11481faf7eb166a6cc97867d
Luiz Filipe <luiz.filipe.vieira@hotmail.com>
2019-10-04 19:43:59 -0300

    meu logout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,17 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { MenuToggleComponent } from './components/menu-toggle/menu-toggle.component';
+import { LogoutButtonComponent } from './components/logout-button/logout-button.component';
 
 @NgModule({
-  declarations: [MenuToggleComponent],
+  declarations: [LogoutButtonComponent, MenuToggleComponent],
   imports: [IonicModule],
-  exports: [CommonModule, ReactiveFormsModule, IonicModule, MenuToggleComponent]
+  exports: [
+    CommonModule,
+    ReactiveFormsModule,
+    IonicModule,
+    LogoutButtonComponent,
+    MenuToggleComponent
+  ]
 })
 export class SharedModule {}
