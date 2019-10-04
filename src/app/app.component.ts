5d6ca16a7d37312dc4eae486ccd6a5c2d59cd224
Luiz Filipe <luiz.filipe.vieira@hotmail.com>
2019-10-04 19:27:25 -0300

    Nome do usuário logado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { AuthService } from './core/services/auth.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -11,8 +13,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent {
   pages: { url: string; direction: string; icon: string; text: string }[];
+  user: firebase.User;
 
   constructor(
+    private authService: AuthService,
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
@@ -25,6 +29,8 @@ export class AppComponent {
       { url: '/tasks', direction: 'back', icon: 'checkmark', text: 'Tasks' },
       { url: '/tasks/create', direction: 'forward', icon: 'add', text: ' New Task' }
     ];
+
+    this.authService.authState$.subscribe(user => (this.user = user));
 
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
