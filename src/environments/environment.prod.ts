22a78bada3b9ca629278f31a49f114cc015a563a
Luiz Filipe <luiz.filipe.vieira@hotmail.com>
2019-10-05 10:57:38 -0300

    configurações para publicar em produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC70jSyeMuOXanTBG36NLO6ams1d-kOw-A',
+    authDomain: 'ionic-firestore-tasks-87ac9.firebaseapp.com',
+    databaseURL: 'https://ionic-firestore-tasks-87ac9.firebaseio.com',
+    projectId: 'ionic-firestore-tasks-87ac9',
+    storageBucket: '',
+    messagingSenderId: '325790575321',
+    appId: '1:325790575321:web:daa45488bb86c6a09fecc8'
+  }
 };
